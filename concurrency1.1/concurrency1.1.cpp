ab4c54d182ec7386c29aacaa2827a646224b8e6f
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-03-03 05:45:59 +0200

    update

--- concurrency1.1/concurrency1.1.cpp
+++ concurrency1.1/concurrency1.1.cpp
@@ -9,7 +9,14 @@
 * of exceptions when using threads
 */
 
-#define EXAMPLE4
+#define EXAMPLE2
+
+
+/* This functions starts a thread
+* then before joining the thread
+* it calls a function that throws 
+*/
+#ifdef EXAMPLE1
 /* a function that does nothing */
 void threadf() {}
 
@@ -18,30 +25,39 @@ void throwf() {
     std::cout << "starting throwf\n";
     throw std::exception{  };
 }
-
-
-#if defined(EXAMPLE2) || defined(EXAMPLE3)
-struct thread_guard {
-    std::thread& _t;
-
-    thread_guard(std::thread& t) :_t(t) { }
-    ~thread_guard() {
-        if(_t.joinable())
-            _t.join();
-        std::cout << "thead guard dtor\n";
-    }
-};
-#endif 
-/* This functions starts a thread
-* then before joining the thread
-* it calls a function that throws 
-*/
-#ifdef EXAMPLE1
 void runt() {
     std::thread t(threadf);
     throwf();
     t.join();
 }
+int main()
+{
+
+    runt();
+    std::cout << "main thread is done\n";
+}
+
+#endif 
+
+#if defined(EXAMPLE2) || defined(EXAMPLE3)
+void threadf() {}
+
+/* a function that throws an exceptions*/
+void throwf() {
+    std::cout << "starting throwf\n";
+    throw std::exception{  };
+}
+
+struct thread_guard {
+    std::thread& _t;
+
+    thread_guard(std::thread& t) :_t(t) { }
+    ~thread_guard() {
+        if (_t.joinable())
+            _t.join();
+        std::cout << "thead guard dtor\n";
+}
+};
 #endif 
 /* The same function but using
 * a thread guard 
@@ -52,6 +68,13 @@ void runt() {
     thread_guard g(t);
     throwf();
 }
+
+int main()
+{
+
+    runt();
+    std::cout << "main thread is done\n";
+}
 #endif 
 #ifdef EXAMPLE3
 void runt() {
@@ -60,6 +83,15 @@ void runt() {
 }
 #endif 
 #ifdef EXAMPLE4
+/* a function that does nothing */
+void threadf() {}
+
+/* a function that throws an exceptions*/
+void throwf() {
+    std::cout << "starting throwf\n";
+    throw std::exception{  };
+}
+
 /* a function wrapper that
 * calls another function
 * that might throw
@@ -74,19 +106,19 @@ void runt(F f) {
 }
 #endif 
 
-
-int main()
-{
-    
-#ifndef EXAMPLE4
-    try {
-        runt();
-    }
-    catch (...) {}
-#else
-    runt(throwf);
-     std::thread t(runt<decltype(throwf)>, throwf);
-     t.join();
-#endif 
-    std::cout << "main thread is done\n";
-}
+//
+//int main()
+//{
+//    
+//#ifndef EXAMPLE4
+//    try {
+//        runt();
+//    }
+//    catch (...) {}
+//#else
+//    runt(throwf);
+//     std::thread t(runt<decltype(throwf)>, throwf);
+//     t.join();
+//#endif 
+//    std::cout << "main thread is done\n";
+//}
--- inclass-3/inclass-3.cpp
+++ inclass-3/inclass-3.cpp
@@ -11,7 +11,7 @@
 #include <random>
 #include <algorithm>
 #include <execution>
-#include "../inclass-2/utitlities.h"
+#include "../include/utility.h"
 
 #define PART2
 /* note that the range is
@@ -42,23 +42,24 @@ void contains(Iter first, Iter last, T value,IterBool result) {
 int main()
 {
 	/* choose size power of two */
-	const int n = 2 << 25;
+	const int n = 1<< 25;
 	std::random_device rd;
 	std::uniform_int_distribution<int> dist(1, 10000);
 	/* choosing a value that is NOT in the vector*/
-	int value = 12000;
+	int value = 20000;
 
 
 	std::vector<int> v(n);
 	std::generate(v.begin(), v.end(), [&]() {return dist(rd); });
 	using Iter = std::vector<int>::iterator;
 	/* using the sequential STL std::any_of() */
-	auto start = std::chrono::high_resolution_clock::now();
-	bool res = std::any_of(v.begin(), v.end(), [=](int x) {return x == value; });
+	Duration d;
+	bool res;
+	TIMEIT(d
+		,res = std::any_of(v.begin(), v.end(), [=](int x) {return x == value; });
+	)
 	std::cout << "sequential result = " << std::boolalpha << res << std::endl;
-	auto end = std::chrono::high_resolution_clock::now();
-	print_duration(start, end, "sequential ");
-
+	std::cout << "duration= " << d.count() << "\n";
 	/* Using our implementation with the number of
 	* threads equal to the hardware threads
 	*/
@@ -69,7 +70,6 @@ int main()
 	std::vector<std::thread> mythreads;
 	Iter begin = v.begin();
 	auto itr = results.begin();
-	start = std::chrono::high_resolution_clock::now();
 	/* declaring i as long long to avoid warning
 	* when we use it for iterator arithmetics
 	* since iterators on x64 are 8 bytes
@@ -77,27 +77,26 @@ int main()
 	/* divide the input into blocks equal to the
 	* number of threads
 	*/
-	for (long long i = 0; i < num_threads; ++i) {
+	TIMEIT(d,
+		for (long long i = 0; i < num_threads; ++i) {
 		/* we can do such arithmetic because
 		* vector iterators are random iterators
 		*/
-		Iter first = begin + i * block_size;
-		Iter last = begin + (i + 1) * block_size;
-		mythreads.push_back(
-			std::thread(
-				contains<Iter, int, decltype(itr)>, first, last, value, itr)
-		);
-		++itr;
-	}
-	for (auto& t : mythreads)
-		t.join();
-	end = std::chrono::high_resolution_clock::now();
-	print_duration(start, end, "multiple threads");
-	auto identity = [](auto r) {return r; };
+			Iter first = begin + i * block_size;
+			Iter last = begin + (i + 1) * block_size;
+			mythreads.push_back(
+				std::thread(
+					contains<decltype(first), int, decltype(itr)>, first, last, value, itr)
+			);
+			++itr;
+		}
+		for (auto& t : mythreads)
+			t.join();
+		
 	/* combine the results of blocks */
-	std::cout<<"multiple thread result "<<
-		std::any_of(results.begin(), results.end(),
-			identity)<< "\n";
-	
+		auto ret=std::find(results.begin(), results.end(), true);
+		std::cout << (ret == results.end() ? "false" : "true") << "\n";
+		)
+		std::cout << "duration= " << d.count() << "\n";
 }
 
